fa09e5bae901df9869c8140a1ee5a3ed07a6d281
Timotej Vaško <133594886+timotejvas@users.noreply.github.com>
2023-05-15 19:45:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-/**
- * TODO: Implement a function that clears all the content
- * prior to generating new random content
- */
+
 function clearAll() {
     const memeContainer = document.querySelector('.meme-content');
     const jokeContainer = document.querySelector('.joke-content');
@@ -15,7 +12,6 @@ function clearAll() {
 }
 
 function showMeme() {
-    // Value is a string representing image URL
     const randomMemeUrl = getRandomData('memes');
     const container = document.querySelector('.meme-content');
     const newImg = document.createElement('img');
@@ -28,13 +24,9 @@ function showMeme() {
 
 }
 
-/**
- * TODO: 
- * - Show a random joke in the correct location
- * - Never show more than 1 joke at a time
- */
+
 function showJoke() {
-    // Value is a string representing the joke
+
     const randomJokeText = getRandomData('jokes');
     const newP = document.createElement('p');
     newP.textContent = randomJokeText;
@@ -45,13 +37,9 @@ function showJoke() {
 
 }
 
-/**
- * TODO: 
- * - Show a random quote in the correct location
- * - Never show more than 1 quote at a time
- */
+
 function showQuote() {
-    // Value should be in format: { quote: '', author: '' }
+
     const randomQuote = getRandomData('quotes');
     const quote = document.createElement('p');
     const author = document.createElement('p');
@@ -64,14 +52,8 @@ function showQuote() {
     container.appendChild(author);
 }
 
-/**
- * TODO: 
- * - Show a random riddle in the correct location
- * - Never show more than 1 riddle at a time
- * - Always hide the riddle's answer initially
- */
+
 function showRiddle() {
-    // Value should be in format: { question: '', answer: '' }
     const randomRiddle = getRandomData('riddles');
 
     const { question, answer } = randomRiddle;
@@ -95,13 +77,7 @@ function showRiddle() {
 
 }
 
-/**
- * TODO: Unhide the riddle's answer
- * - If there is no riddle shown, alert the user that there is no riddle
- * - If there is a riddle shown and an answer shown, alert the user
- *   that the answer is already revealed
- * - If there is a riddle shown but no answer, unhide the answer!
- */
+
 function revealAnswers() {
     const riddleContainer = document.querySelector('.riddle-content');
     const riddle = riddleContainer.querySelector('p');
@@ -116,46 +92,16 @@ function revealAnswers() {
     }
 }
 
-/**
- * This function is used to get random data.  Don't worry about how it works, just know how to use it.  Usage is pre-filled in the functions above already, but here's an explanation of the function anyways.
- * 
- * Valid arguments:
- *
- * 'memes', 'jokes', 'quotes', 'riddles'
- *
- * Return values:
- * 
- * For meme data: 
- * A string representing an image url
- * 
- * For joke data: 
- * A string representing the joke
- * 
- * For quote data:
- * An object - { quote: '', author: '' }
- * 
- * For riddle data:
- * An object - { question: '', answer: '' }
- *
- * Example usage: getRandomData('quotes');
- */
+
 function getRandomData(type) {
     return data[type][rn(data[type].length)];
 }
 
-// ----------------------------------------------------
-// NO NEED TO CHANGE ANYTHING BELOW but...
-// feel free to add/remove items from these lists to customize
-// your results
-// ----------------------------------------------------
-
-// Source: https://www.thecoderpedia.com/blog/programming-memes/, Reddit
 const memes = ['https://i.redd.it/a0v87gwzoge61.jpg', 'https://i.redd.it/q29egav34ee61.jpg', 'https://i.redd.it/iij16swxjie61.jpg', 'https://i.redd.it/vek7dm2hrge61.jpg', 'https://www.testbytes.net/wp-content/uploads/2019/06/Untitled-8.png', 'https://miro.medium.com/max/1000/0*Ua695vjzFHV6VNOX.png', 'https://pbs.twimg.com/media/EKkPagPXkAA__Qo.jpg', 'https://code-love.com/wp-content/uploads/2019/03/download.jpeg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Programmer-while-sleeping.jpg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Evolution-of-Memory-Storage-1024x996.jpg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Error-in-Code-896x1024.jpg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Coding-Meme-Code-Comments-be-Like-925x1024.jpg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Internet-Explorer-Joke-915x1024.jpg'];
 
-// Sourced from: http://www.devtopics.com/best-programming-jokes/
 const jokes = ['This statement', 'Eight bytes walk into a bar.  The bartender asks, “Can I get you anything?” “Yeah,” reply the bytes.  “Make us a double.”', 'There are only 10 kinds of people in this world: those who know binary and those who don’t.', 'All programmers are playwrights, and all computers are lousy actors.', 'Have you heard about the new Cray super computer?  It’s so fast, it executes an infinite loop in 6 seconds.', 'The generation of random numbers is too important to be left to chance.', 'Debugging: Removing the needles from the haystack.', '“Debugging” is like being the detective in a crime drama where you are also the murderer.', 'There are two ways to write error-free programs; only the third one works.', 'The best thing about a Boolean is even if you are wrong, you are only off by a bit.'];
 
-// source: https://www.goodreads.com/quotes/tag/programming
+
 const quotes = [
     { quote: 'Programs must be written for people to read, and only incidentally for machines to execute.', author: 'Harold Abelson' },
     { quote: 'Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.', author: 'Rick Cook' },
@@ -171,7 +117,6 @@ const quotes = [
     { quote: 'No matter which field of work you want to go in, it is of great importance to learn at least one programming language.', author: 'Ram Ray' },
 ];
 
-// Source: https://www.rd.com/list/challenging-riddles/
 const riddles = [
     { question: 'I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I?', answer: 'An echo' },
     { question: 'You measure my life in hours and I serve you by expiring. I’m quick when I’m thin and slow when I’m fat. The wind is my enemy. ', answer: 'A Candle' },
@@ -181,7 +126,6 @@ const riddles = [
     { question: 'What word in the English language does the following: the first two letters signify a male, the first three letters signify a female, the first four letters signify a great, while the entire world signifies a great woman. What is the word?', answer: 'Heroine' }
 ];
 
-// Just a little helper function
 function rn(len) {
     return Math.floor(Math.random() * len);
 }
